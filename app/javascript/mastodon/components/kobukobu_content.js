e4913173fc419690b58a0eb051ee6895f237d471
Chris Carrington <chris@megafarad.com>
2022-12-05 15:59:32 -0800

    No furigana for katakana

--- app/javascript/mastodon/components/kobukobu_content.js
+++ app/javascript/mastodon/components/kobukobu_content.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { fit } from "furigana";
-import { toHiragana, isJapanese, isKana, isKanji } from "wanakana";
+import { toHiragana, isJapanese, isKana, isKatakana } from "wanakana";
 import ImmutablePropTypes from "react-immutable-proptypes";
 import PropTypes from "prop-types";
 
@@ -19,8 +19,9 @@ class KobuKobuContent extends React.PureComponent {
     if (kobukobuJS.language === 'ja') {
       return(<p>
         {kobukobuJS.words.map((word, i) =>
-          !isJapanese(word.word) || !isKana(word.transcription) ? word.word : this.furigana(word) ?
-            this.furigana(word).map((match, j) => match.w === match.r ? match.r : <ruby>{match.w}<rt>{match.r}</rt></ruby>) : word.word
+          !isJapanese(word.word) || !isKana(word.transcription) || isKatakana(word.word) ? word.word : this.furigana(word) ?
+            this.furigana(word).map((match, j) =>
+              match.w === match.r ? match.r : <ruby>{match.w}<rt>{match.r}</rt></ruby>) : word.word
         )}
       </p>);
     } else {
